3b92fcb81acc217bfb25fd412d6b6c9448cd1b77
yumiova <yumiova@protonmail.ch>
2019-06-02 19:44:29 +0200

    Rename 'p_instNameDecl'.

--- src/Ormolu/Printer/Meat/Declaration/Instance.hs
+++ src/Ormolu/Printer/Meat/Declaration/Instance.hs
@@ -20,13 +20,13 @@ p_clsInstDecl :: ClsInstDecl GhcPs -> R ()
 p_clsInstDecl = \case
   ClsInstDecl {..} -> do
     txt "instance "
-    p_instNameDecl cid_poly_ty
+    p_hsSigType cid_poly_ty
     txt " where"
     breakpoint
     forM_ cid_binds $ \c -> inci (located c p_valDecl)
   XClsInstDecl NoExt -> notImplemented "XClsInstDecl"
 
-p_instNameDecl :: LHsSigType GhcPs -> R ()
-p_instNameDecl = \case
+p_hsSigType :: LHsSigType GhcPs -> R ()
+p_hsSigType = \case
   HsIB {..} -> located hsib_body p_hsType
   XHsImplicitBndrs NoExt -> notImplemented "XHsImplicitBndrs"
