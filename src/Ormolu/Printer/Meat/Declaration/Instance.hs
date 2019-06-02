0ba8a37a7672bd8742df786c21fb7ff801b00929
yumiova <yumiova@protonmail.ch>
2019-06-02 04:24:49 +0200

    Generate instance signature & name.

--- src/Ormolu/Printer/Meat/Declaration/Instance.hs
+++ src/Ormolu/Printer/Meat/Declaration/Instance.hs
@@ -11,12 +11,19 @@ where
 
 import GHC
 import Ormolu.Printer.Combinators
+import Ormolu.Printer.Meat.Type
 import Ormolu.Utils
 
 p_clsInstDecl :: ClsInstDecl GhcPs -> R ()
 p_clsInstDecl = \case
   ClsInstDecl {..} -> do
     txt "instance "
+    p_instNameDecl cid_poly_ty
     txt " where"
     breakpoint
   _ -> notImplemented "certain types of signature declarations"
+
+p_instNameDecl :: LHsSigType GhcPs -> R ()
+p_instNameDecl = \case
+  HsIB {..} -> located hsib_body p_hsType
+  XHsImplicitBndrs NoExt -> notImplemented "XHsImplicitBndrs"
