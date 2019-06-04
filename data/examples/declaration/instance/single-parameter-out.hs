a90a00f279a5ee9ef075cbf8da8f5424d7b617c7
yumiova <yumiova@protonmail.ch>
2019-06-04 23:28:25 +0200

    Add tests for multi-parameter type class instances.

--- data/examples/declaration/instance/single-parameter-out.hs
+++ data/examples/declaration/instance/single-parameter-out.hs
@@ -2,4 +2,6 @@ instance Monoid Int where (<>) x y = x + y
 
 instance Enum Int where
   fromEnum x = x
-  toEnum = \x -> x
+  toEnum =
+    \x ->
+      x
--- data/examples/declaration/instance/single-parameter.hs
+++ data/examples/declaration/instance/single-parameter.hs
@@ -3,4 +3,6 @@ instance Monoid Int where (<>) x y = x+y
 instance Enum Int
   where
     fromEnum x = x
-    toEnum = \x -> x
+    toEnum
+      = \x ->
+        x
