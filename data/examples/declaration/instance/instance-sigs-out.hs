9fcab338c1482261d65f3cbc6f928624ee723d5e
yumiova <yumiova@protonmail.ch>
2019-06-04 23:55:14 +0200

    Add tests for instance signatures.

---
+++ data/examples/declaration/instance/instance-sigs-out.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE InstanceSigs #-}
+instance Eq Int where
+  (==) :: Int -> Int -> Bool
+  (==) _ _ = False
+
+instance Ord Int where
+  compare
+    :: Int
+    -> Int
+    -> Ordering
+  compare
+    _
+    _ = GT
---
+++ data/examples/declaration/instance/instance-sigs.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE InstanceSigs #-}
+
+instance Eq Int
+  where
+    (==) :: Int -> Int -> Bool
+    (==) _ _ = False
+
+instance Ord Int where
+    compare ::
+           Int
+        -> Int
+        -> Ordering
+    compare
+        _
+        _
+        = GT
