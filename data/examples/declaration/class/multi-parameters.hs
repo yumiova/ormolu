d880944b3fbd5c49b1b156ee1284b13f0bd839d5
yumiova <yumiova@protonmail.ch>
2019-06-07 19:01:41 +0200

    Add tests for multi-parameter type classes.

---
+++ data/examples/declaration/class/multi-parameters.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+class Foo a b where foo :: a -> b
+
+class Bar
+        a b c d
+  where
+    bar ::
+           a
+        -> b
+        -> c
+        -> d
+
+class Baz
+  where
+    baz :: Int
+
+class
+      BarBaz
+        a
+        b
+        c
+        d
+        e
+        f where
+    barbaz ::
+        a -> f
+    bazbar ::
+        e ->
+        f
