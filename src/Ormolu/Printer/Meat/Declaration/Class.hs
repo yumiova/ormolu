b31812b7df15f969b03540f74784ca443a2730fb
yumiova <yumiova@protonmail.ch>
2019-06-07 17:25:55 +0200

    Add pretty printing for type class name & variables.

--- src/Ormolu/Printer/Meat/Declaration/Class.hs
+++ src/Ormolu/Printer/Meat/Declaration/Class.hs
@@ -9,10 +9,18 @@ module Ormolu.Printer.Meat.Declaration.Class
   )
 where
 
+import Control.Monad
 import Ormolu.Printer.Combinators
+import Ormolu.Printer.Meat.Common
+import Ormolu.Printer.Meat.Type
 import GHC
 import RdrName (RdrName (..))
 import SrcLoc (Located)
 
 p_classDecl :: Located RdrName -> LHsQTyVars GhcPs -> R ()
-p_classDecl _ _ = pure ()
+p_classDecl name tvars = do
+  let HsQTvs {..} = tvars
+  txt "class "
+  p_rdrName name
+  unless (null hsq_explicit) space
+  spaceSep (located' p_hsTyVarBndr) hsq_explicit
