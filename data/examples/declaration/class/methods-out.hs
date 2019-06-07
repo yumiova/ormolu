52d2134eff1c422273b86e9478b7e6109858f7d1
yumiova <yumiova@protonmail.ch>
2019-06-07 18:22:13 +0200

    Add tests for pretty printing of type class signatures.

---
+++ data/examples/declaration/class/methods-out.hs
@@ -0,0 +1,18 @@
+class Foo a where
+  foo :: a
+
+class Bar a where
+  bar :: a -> a -> a
+
+class Baz a where
+  baz
+    :: ( a
+       , a
+       )
+    -> a
+    -> a
+
+class BarBaz a where
+  barbaz
+    :: a -> b
+  bazbar :: b -> a
