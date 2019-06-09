b35cdb8d1e41035b2f588874874328f41c0e53c7
yumiova <yumiova@protonmail.ch>
2019-06-09 21:33:58 +0200

    Add tests for pretty printing of default signatures.

---
+++ data/examples/declaration/class/default-signatures.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE DefaultSignatures #-}
+
+class Foo a where
+    foo :: a -> String
+    default foo :: Show a => a -> String
+    foo = show
+
+class Bar a
+  where
+    bar ::
+           String
+        -> String
+        -> a
+
+    default bar :: (
+        Read a,
+        Semigroup a
+      ) =>
+      a -> a -> a
+
+    bar
+      a
+      b
+      =
+      read a <> read b
